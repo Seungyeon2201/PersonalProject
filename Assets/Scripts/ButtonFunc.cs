c091af0fabb0cfc0c9d047db00a5fd35d4e44bca
Seungyeon2201 <ksy9387@naver.com>
2022-11-19 17:20:04 +0900

    refactor : 주석 추가
    주석 추가

--- Assets/Scripts/ButtonFunc.cs
+++ Assets/Scripts/ButtonFunc.cs
@@ -11,16 +11,20 @@ public class ButtonFunc : MonoBehaviour
     {
         button = GetComponent<Button>();
     }
+    //몬스터 사면 몬스터 소환 및 버튼 비활성화
     public void Buy()
     {
         StoreManager.Instance.BuyMonster(monsterType);
         button.interactable = false;
     }
 
+    //몬스터 팔기
     public void Sell()
     {
         Grabber.Instance.ClickForSell();
     }
+
+    //상점 리롤시 버튼 활성화
     public void OnInteractable()
     {
         button.interactable = true;
